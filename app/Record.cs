742b12efb011a18cd9e81bc13bf85689ffb868a0
lexasss <oleg.spakov@tuni.fi>
2025-01-31 11:14:37 +0200

    NBackTask event are parsed and inlcuded into Record

--- app/Record.cs
+++ app/Record.cs
@@ -3,12 +3,26 @@
 public record class Rotation(double Pitch, double Yaw, double Roll);
 public record class Vector3D(double X, double Y, double Z);
 public record class Pupil(double Openness, double Size);
+
+public enum NBackTaskEventType
+{
+    SessionStart,
+    TrialStart,
+    Response,
+    TrialEnd,
+    SessionEnd
+}
+public record class NBackTaskEvent(NBackTaskEventType Type);
+public record class NBackTaskTrial(NBackTaskEventType Type, int ID) : NBackTaskEvent(Type);
+public record class NBackTaskTrialResult(NBackTaskEventType Type, int ID, bool IsSuccess) : NBackTaskTrial(Type, ID);
+
 public record class Record(
     long TimestampSystem,
     long TimestampHeadset,
     Rotation Eye, Rotation Head,
     Pupil LeftPupil, Pupil RightPupil,
-    Vector3D HandPalm, Vector3D HandThumb, Vector3D HandIndex, Vector3D HandMiddle)
+    Vector3D HandPalm, Vector3D HandThumb, Vector3D HandIndex, Vector3D HandMiddle,
+    NBackTaskEvent? NBackTaskEvent)
 {
     public static Record? Parse(string? text)
     {
@@ -31,7 +45,17 @@ public record class Record(
                 new Vector3D(double.Parse(p[10]), double.Parse(p[11]), double.Parse(p[12])),
                 new Vector3D(double.Parse(p[13]), double.Parse(p[14]), double.Parse(p[15])),
                 new Vector3D(double.Parse(p[16]), double.Parse(p[17]), double.Parse(p[18])),
-                new Vector3D(double.Parse(p[19]), double.Parse(p[20]), double.Parse(p[21]))
+                new Vector3D(double.Parse(p[19]), double.Parse(p[20]), double.Parse(p[21])),
+                string.IsNullOrEmpty(p[22]) ? null :
+                    p[22].Split(' ') switch
+                    {
+                        ["STR"] => new NBackTaskEvent(NBackTaskEventType.SessionStart),
+                        ["SET", var id] => new NBackTaskTrial(NBackTaskEventType.TrialStart, int.Parse(id)),
+                        ["ACT", var id] => new NBackTaskTrial(NBackTaskEventType.Response, int.Parse(id)),
+                        ["RES", var id, var isSuccess] => new NBackTaskTrialResult(NBackTaskEventType.Response, int.Parse(id), bool.Parse(isSuccess)),
+                        ["FIN"] => new NBackTaskEvent(NBackTaskEventType.SessionEnd),
+                        _ => throw new Exception($"Unknown NBackTask event: {p[22]}")
+                    }
             );
         }
         catch
